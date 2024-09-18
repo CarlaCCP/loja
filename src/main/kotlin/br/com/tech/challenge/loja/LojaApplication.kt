e4cb2817ca4fdf4efe1beaa9cbcc5e6baee28f5f
carla-gb <carla.paula@grupoboticario.com.br>
2024-09-18 16:03:25 -0300

    adiciona workflow

--- src/main/kotlin/br/com/tech/challenge/loja/LojaApplication.kt
+++ src/main/kotlin/br/com/tech/challenge/loja/LojaApplication.kt
@@ -2,13 +2,14 @@ package br.com.tech.challenge.loja
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 import org.springframework.scheduling.annotation.EnableScheduling
-
 
 @EnableMongoRepositories
 @SpringBootApplication
 @EnableScheduling
+@EnableFeignClients
 class LojaApplication
 
 fun main(args: Array<String>) {
