8eca0e45610ddf75a46885be7ddfbec4d60e216f
carla-gb <carla.paula@grupoboticario.com.br>
2024-09-30 23:25:46 -0300

    deploy api

--- src/main/kotlin/br/com/tech/challenge/loja/config/DynamoDBConfig.kt
+++ src/main/kotlin/br/com/tech/challenge/loja/config/DynamoDBConfig.kt
@@ -1,10 +1,9 @@
 package br.com.tech.challenge.loja.config
 
+
 import br.com.tech.challenge.loja.interfaces.gateway.IClientGateway
 import br.com.tech.challenge.loja.interfaces.gateway.IOrderGateway
 import br.com.tech.challenge.loja.interfaces.gateway.IProductGateway
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
-import com.amazonaws.auth.InstanceProfileCredentialsProvider
 import com.amazonaws.regions.Regions
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
@@ -25,7 +24,14 @@ class DynamoDBConfig{
   fun amazonDynamoDB(): AmazonDynamoDB = AmazonDynamoDBClientBuilder
     .standard()
     .withRegion(Regions.US_EAST_1)
-    .withCredentials(DefaultAWSCredentialsProviderChain.getInstance())
     .build()
+
+//  private fun awsCredentialsProvider(): AWSCredentialsProvider =
+//    AWSStaticCredentialsProvider(awsCredentials())
+//
+//  private fun awsCredentials(): AWSCredentials = BasicAWSCredentials(
+//    awsProperties.accessKey,
+//    awsProperties.secretKey
+//  )
 
 }
