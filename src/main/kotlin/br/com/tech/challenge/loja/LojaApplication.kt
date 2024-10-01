a55d34beef03f12fc57dd9df7aaf36636845a8fa
carla-gb <carla.paula@grupoboticario.com.br>
2024-09-30 22:55:52 -0300

    deploy api

--- src/main/kotlin/br/com/tech/challenge/loja/LojaApplication.kt
+++ src/main/kotlin/br/com/tech/challenge/loja/LojaApplication.kt
@@ -1,8 +1,6 @@
 package br.com.tech.challenge.loja
 
-import br.com.tech.challenge.loja.config.properties.AwsProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.scheduling.annotation.EnableScheduling
@@ -10,7 +8,6 @@ import org.springframework.scheduling.annotation.EnableScheduling
 @SpringBootApplication
 @EnableScheduling
 @EnableFeignClients
-@EnableConfigurationProperties(AwsProperties::class)
 class LojaApplication
 
 fun main(args: Array<String>) {
