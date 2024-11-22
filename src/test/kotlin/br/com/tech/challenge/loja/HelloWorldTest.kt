a7922d668006dff01240cea1438883a44ce01fa6
carla-gb <carla.paula@grupoboticario.com.br>
2024-11-22 18:42:09 -0300

    adiciona coverage

--- src/test/kotlin/br/com/tech/challenge/loja/HelloWorldTest.kt
+++ src/test/kotlin/br/com/tech/challenge/loja/HelloWorldTest.kt
@@ -1,10 +1,12 @@
 package br.com.tech.challenge.loja
 
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
 class HelloWorldTest {
 
   @Test
+  @DisplayName("HELLOOOO")
   fun oi() {
     val expected = 42
     assert(expected == 42)
