769f95956a737fdb8ff6d9eb6086d0dd82f2b5b8
carla-gb <carla.paula@grupoboticario.com.br>
2024-11-24 19:34:39 -0300

    adiciona kotest e teste

---
+++ src/test/kotlin/br/com/tech/challenge/loja/fixture/OrderGetAdapterFixture.kt
@@ -0,0 +1,9 @@
+package br.com.tech.challenge.loja.fixture
+
+import br.com.tech.challenge.loja.adapter.OrderGetAdapter
+
+class OrderGetAdapterFixture {
+
+  fun orderAdapter() =
+    OrderGetAdapter
+}
