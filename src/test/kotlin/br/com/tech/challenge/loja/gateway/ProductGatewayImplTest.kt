f6240ceba49236eec998d054e1db19a927be141d
carla-gb <carla.paula@grupoboticario.com.br>
2024-11-29 11:07:53 -0300

    adiciona testes

--- src/test/kotlin/br/com/tech/challenge/loja/gateway/ProductGatewayImplTest.kt
+++ src/test/kotlin/br/com/tech/challenge/loja/gateway/ProductGatewayImplTest.kt
@@ -1,30 +1,27 @@
 package br.com.tech.challenge.loja.gateway
 
+import br.com.tech.challenge.loja.core.entity.product.Product
 import br.com.tech.challenge.loja.core.valueObject.category.Category
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
 import com.amazonaws.services.dynamodbv2.document.*
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.nulls.shouldNotBeNull
 import com.amazonaws.services.dynamodbv2.model.*
-import io.kotest.engine.runBlocking
-import io.kotest.matchers.reflection.beLateInit
 import io.mockk.*
 
 class ProductGatewayImplTest : FunSpec({
 
   lateinit var dynamoDBClient: AmazonDynamoDB
-  lateinit var dynamoDb: DynamoDB
+
   lateinit var productGatewayImpl: ProductGatewayImpl
 
 
   beforeTest {
     dynamoDBClient = mockk<AmazonDynamoDB>()
-    dynamoDb = mockk<DynamoDB>()
 
     productGatewayImpl = ProductGatewayImpl(dynamoDBClient)
 
   }
-
 
   test("Should find product by category") {
     val queryRequest = QueryRequest()
